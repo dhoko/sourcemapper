ed4e92f7e7c67b8467eebabaf9bb1a7052ad571d
dhoko <dhoko@pm.me>
2023-11-07 09:12:49 +0100

    Build sourcemapper for Apple Arm

--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ export GOBIN="$(pwd)/bin"
 go install main.go
 
 if [[ "$OSTYPE" = "darwin"* ]]; then
+  if [[ "$(uname -m)" == 'arm64'  ]]; then
+    mv bin/main bin/isourcemapper-arm
+    exit 0
+  fi
+    
   mv bin/main bin/isourcemapper
 else
   mv bin/main bin/sourcemapper
